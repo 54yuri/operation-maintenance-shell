e56143f1fa9b9c392efed324627bc11b824e7640
54yuri <54yuri@163.com>
2014-11-22 22:28:12 +0800

    Create batch_tlog.sh

---
+++ batch_tlog.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -lt 4 ]; then
+	echo "usage: $0 \$user \$passwd \$cmdFile \$tmplFile [\$outFile]"
+	exit
+fi
+
+user=$1
+passwd="$2"
+cmdFile=$3
+tmplFile=$4
+outFile=$5
+
+tmpCmdFile="$cmdFile".real
+cp $cmdFile $tmpCmdFile
+
+cat $tmplFile | grep -v IP | while read IP worldid
+do
+	cat $cmdFile | sed "s/{{WORLDID}}/$worldid/g" > $tmpCmdFile
+	sh remote_cmd.sh $IP $user "$passwd" $tmpCmdFile "$outFile${worldid}"
+done
+
+rm $tmpCmdFile
+
